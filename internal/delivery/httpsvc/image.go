120f05a99fe5862a6da6e97192746edaba22505c
luckyAkbar <luckyakbar1509@gmail.com>
2022-04-17 15:52:33 +0700

    v0.1.0-feature/save-image

--- internal/delivery/httpsvc/image.go
+++ internal/delivery/httpsvc/image.go
@@ -1,6 +1,7 @@
 package httpsvc
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -8,6 +9,7 @@ import (
 
 func (s *Service) handleSave() echo.HandlerFunc {
 	return func(c echo.Context) error {
+		fmt.Println(c.RealIP())
 		return c.JSON(http.StatusOK, "hello world")
 	}
 }
